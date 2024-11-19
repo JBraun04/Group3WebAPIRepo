186a424e27d6741c287c0c5c96f5ea675e5356cc
Jack Braun <j.braun3456@gmail.com>
2024-11-19 13:27:04 -0500

    Modified the Movie.cs model and created the controller.

--- Data/Group3WebAPIContext.cs
+++ Data/Group3WebAPIContext.cs
@@ -15,5 +15,6 @@ namespace Group3WebAPI.Data
         }
 
         public DbSet<Group3WebAPI.Models.Breakfast> Breakfast { get; set; } = default!;
+        public DbSet<Group3WebAPI.Models.Movie> Movie { get; set; } = default!;
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -2,12 +2,12 @@
 {
     public class Movie
     {
-        public int Id;
-        public string title;
-        public string genre;
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
         //length in MINUTES
-        public int length;
-        public int releaseYear;
+        public int length { get; set; }
+        public int releaseYear { get; set; }
 
         public Movie() { }
         public Movie(int id, string title, string genre, int length, int releaseYear)
