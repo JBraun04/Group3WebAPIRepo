1c48e523665b5178c4578a22d22d890a2b4b9fd1
Jack Braun <j.braun3456@gmail.com>
2024-11-19 13:22:14 -0500

    Changed a few things with the Movie.cs model

---
+++ Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿namespace Group3WebAPI.Models
+{
+    public class Movie
+    {
+        public int Id;
+        public string title;
+        public string genre;
+        //length in MINUTES
+        public int length;
+        public int releaseYear;
+
+        public Movie() { }
+        public Movie(int id, string title, string genre, int length, int releaseYear)
+        {
+            this.Id = id;
+            this.title = title;
+            this.genre = genre;
+            this.length = length;
+            this.releaseYear = releaseYear;
+        }
+
+    }
+}
