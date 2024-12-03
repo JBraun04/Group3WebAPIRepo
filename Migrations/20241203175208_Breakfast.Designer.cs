96a7bce396c829ec913b0fa2f9026d558b76a376
Rodzell Tucker <tuckerr8@mail.uc.edu>
2024-12-03 13:41:30 -0500

    Reapply "Rodzell's working scripts for Breakfast, Movie, and TeamMember. Hobby did not build a table on his machine."
    This reverts commit bfe6c65907d91bd359d6e9df25c84a0f94832016.

--- Migrations/20241203175208_Breakfast.Designer.cs
+++ Migrations/20241203175208_Breakfast.Designer.cs
@@ -3,6 +3,7 @@ using Group3WebAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Group3WebAPI.Migrations
 {
     [DbContext(typeof(Group3WebAPIContext))]
-    partial class Group3WebAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20241203175208_Breakfast")]
+    partial class Breakfast
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
